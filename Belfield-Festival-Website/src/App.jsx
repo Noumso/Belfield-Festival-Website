ac7f0c8df25bbb642739ae9ce459ed523e8f53df
Noumso <159029742+Noumso@users.noreply.github.com>
2025-05-07 12:09:01 +0000

    Refactor App component structure by removing Router wrapper and simplifying layout

--- Belfield-Festival-Website/src/App.jsx
+++ Belfield-Festival-Website/src/App.jsx
@@ -1,24 +1,21 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import Layout from "./components/Layout";
-import Schedule from './pages/Schedule';
-import Info from './pages/Info';
-import Admin from './pages/Admin';
-import './App.css';
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Schedule from './pages/Schedule'
+import Info from './pages/Info'
+import Admin from './pages/Admin'
+import './App.css'
 
 function App() {
   return (
-    <Router>
-      <header>
-        <nav>
-          <ul className="nav">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/schedule">Schedule</Link></li>
-            <li><Link to="/info">Info</Link></li>
-            <li><Link to="/admin">Admin</Link></li>
-          </ul>
-        </nav>
-      </header>
+    <div>
+      <nav>
+        <ul className="nav">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/schedule">Schedule</Link></li>
+          <li><Link to="/info">Info</Link></li>
+          <li><Link to="/admin">Admin</Link></li>
+        </ul>
+      </nav>
 
       <main>
         <Routes>
@@ -28,8 +25,8 @@ function App() {
           <Route path="/admin" element={<Admin />} />
         </Routes>
       </main>
-    </Router>
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
