e22ef3d90955c5b36e219d9174b41b0ac0dfcc5d
Noumso <159029742+Noumso@users.noreply.github.com>
2025-05-07 12:16:24 +0000

    Refactor App component to use Router and Layout for improved routing structure

--- Belfield-Festival-Website/src/App.jsx
+++ Belfield-Festival-Website/src/App.jsx
@@ -1,32 +1,23 @@
-import { Routes, Route, Link } from 'react-router-dom'
-import Home from './pages/Home'
-import Schedule from './pages/Schedule'
-import Info from './pages/Info'
-import Admin from './pages/Admin'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './layout/Layout';
+import Home from './pages/Home';
+import Schedule from './pages/Schedule';
+import Info from './pages/Info';
+import Admin from './pages/Admin';
 
 function App() {
   return (
-    <div>
-      <nav>
-        <ul className="nav">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/schedule">Schedule</Link></li>
-          <li><Link to="/info">Info</Link></li>
-          <li><Link to="/admin">Admin</Link></li>
-        </ul>
-      </nav>
-
-      <main>
-        <Routes>
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/schedule" element={<Schedule />} />
           <Route path="/info" element={<Info />} />
           <Route path="/admin" element={<Admin />} />
-        </Routes>
-      </main>
-    </div>
-  )
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
