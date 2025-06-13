6f574f026cb652ef091d36b391dd5b6c517f0f16
Noumso <159029742+Noumso@users.noreply.github.com>
2025-06-13 09:34:57 +0000

    Add environment variable check for MONGO_URI in db.js

--- belfield-festival-website/db.js
+++ belfield-festival-website/db.js
@@ -1,6 +1,12 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import process from 'node:process';
 dotenv.config();
+
+// Ensure MONGO_URI is defined
+if (typeof globalThis.process !== 'undefined' && !globalThis.process.env.MONGO_URI) {
+  throw new Error('MONGO_URI is not defined in the environment variables');
+}
 
 export async function connectDB() {
   try {
