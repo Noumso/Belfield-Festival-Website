9a0520a07497c4aa23f70b424e04e76f5c01a721
Noumso <159029742+Noumso@users.noreply.github.com>
2025-06-19 09:24:26 +0000

    Refactor application structure: add ProtectedRoute component, update routing, and implement Artists page; remove unused components and styles

--- belfield-festival-website/client/src/App.jsx
+++ belfield-festival-website/client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import ProtectedRoute from './components/ProtectedRoute';
 import Home from './pages/Home';
 import Schedule from './pages/Schedule';
 import Gallery from './pages/Gallery';
 import Info from './pages/Info';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
-import Navbar from './components/Navbar';
+import Artists from './pages/Artists';
 
 function App() {
   return (
@@ -14,11 +16,19 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/programme" element={<Schedule />} />
-        <Route path="/galerie" element={<Gallery />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/gallery" element={<Gallery />} />
         <Route path="/info" element={<Info />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<Dashboard />} />
+        <Route path="/artists" element={<Artists />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
