f9592fc92f297280505991b3bebe5106cea6c3c3
Noumso <159029742+Noumso@users.noreply.github.com>
2025-10-06 13:31:50 +0000

    Remove unused assets, environment variables, and server routes for project cleanup

--- belfield-festival-website/routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.send('Bienvenue sur le site officiel du Belfield Festival !');
-});
-
-export default router;
